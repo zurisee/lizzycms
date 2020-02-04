79692b03753ae2a8f19fc3e8ec7bd2d8f6d57651
sto <sto@usility.ch>
2020-02-04 10:26:22 +0100

    generic function isValidEmail() added

--- js/auxiliary.js
+++ js/auxiliary.js
@@ -19,6 +19,13 @@
 
 
 
+function isValidEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
 //--------------------------------------------------------------
 function scrollIntoView( selector, container )
 {
