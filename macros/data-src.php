1349c9c24396c5fea7c37344c30796fb842fdbcb
Dieter Stokar <zurisee@gmail.com>
2020-04-04 16:20:23 +0200

    new macro data-src() - so far just to return size of data source - more to come if useful

---
+++ macros/data-src.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$macroName = basename(__FILE__, '.php');    // macro name normally the same as the file name
+$this->addMacro($macroName, function () {
+	$macroName = basename(__FILE__, '.php');
+	$this->invocationCounter[$macroName] = (!isset($this->invocationCounter[$macroName])) ? 0 : ($this->invocationCounter[$macroName]+1);
+    $inx = $this->invocationCounter[$macroName] + 1;
+
+    $dataSrc = $this->getArg($macroName, 'dataSrc', 'Filename of datasource', '');
+    $return = $this->getArg($macroName, 'return', '[size|recNo] ', false);
+
+    if (!$dataSrc || ($dataSrc === 'help')) {
+        return '';
+    }
+    $dataSrc = makePathRelativeToPage($dataSrc);
+    $db = new DataStorage2( $dataSrc);
+
+    $str = '';
+    if ($return === 'size') {
+        $str = $db->getSize();
+    }
+
+    $this->optionAddNoComment = true;
+	return $str;
+});
