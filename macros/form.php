966aea42d85ff1652c91b80bbc1333f22a2e9d3c
Dieter Stokar <zurisee@gmail.com>
2020-09-18 11:10:35 +0200

    macro form(): -> modified to use derived class -> functionality unchanged

--- macros/form.php
+++ macros/form.php
@@ -12,87 +12,101 @@ $this->addMacro($macroName, function () {
 	$inx = $this->invocationCounter[$macroName] + 1;
 
     $this->disablePageCaching = $this->getArg($macroName, 'disableCaching', '(false) Enables page caching (which is disabled for this macro by default). Note: only active if system-wide caching is enabled.', true);
-    $headArgs = $this->getArgsArray($macroName);
-    if (isset($headArgs['disableCaching'])) {
-        unset($headArgs['disableCaching']);
+    $args = $this->getArgsArray($macroName);
+    if (isset($args['disableCaching'])) {
+        unset($args['disableCaching']);
     }
 
-    if (@$headArgs[0] === 'help') {
+    $form = new Form($this->lzy, $inx);
+
+    if (@$args[0] === 'help') {
         $this->compileMd = true;
-        return renderHelp();
+        return $form->renderHelp();
     }
-
-
-    if ($inx === 1) {
-        $this->form = new Forms($this->lzy, $inx);
-    }
-
-    $headElements = ['formName', 'id', 'mailto', 'mailfrom', 'mailTo', 'mailFrom', 'legend', 'customResponseEvaluation', 'next',
-        'confirmationText', 'file', 'warnLeavingPage', 'options', 'formTimeout', 'export', 'prefill',
-        'preventMultipleSubmit', 'replaceQuotes', 'antiSpam', 'validate', 'showData',
-        'showDataMinRows', 'encapsulate', 'disableCaching'];
-
-    // separate arguments for header and fields:
-    $headArgs = $this->getArgsArray($macroName);
-    $formElems = [];
-    foreach ($headArgs as $key => $value) {
-        if (!in_array($key, $headElements)) {
-            $formElems[$key] = $value;
-            unset($headArgs[$key]);
-        }
-    }
-
-
-    // create form head:
-    $headArgs['type'] = 'form-head';
-    $str = $this->form->render( $headArgs );
-
-    // create form buttons:
-    $buttons = [ 'label' => '', 'type' => 'button', 'value' => '' ];
-
-    // parse further arguments, interpret as form field definitions:
-    foreach ($formElems as $label => $arg) {
-        if (is_string($arg)) {
-            $arg = ['type' => $arg ? $arg : 'text'];
-        }
-        if (isset($arg[0])) {
-            if ($arg[0] === 'required') {
-                $arg['required'] = true;
-                unset($arg[0]);
-            } else {
-                $arg['type'] = $arg[0];
-            }
-        }
-        if ($label === 'submit') {
-            $buttons["label"] .= isset($arg['label']) ? $arg['label'].',': '{{ Submit }},';
-            $buttons["value"] .= 'submit,';
-            $arg['type'] = 'button';
-
-        } elseif ($label === 'cancel') {
-            $buttons["label"] .= isset($arg['label']) ? $arg['label'].',': '{{ Cancel }},';
-            $buttons["value"] .= 'cancel,';
-            $arg['type'] = 'button';
-
-        } elseif (strpos('formName,mailto,mailfrom,legend,showData', $label) !== false) {
-        } else {
-            $arg['label'] = $label;
-            $str .= $this->form->render($arg);
-        }
-    }
-    if ($buttons["label"]) {
-        $str .= $this->form->render($buttons);
-    }
-
-    $str .= $this->form->render([ 'type' => 'form-tail' ]);
+    $str = $form->render( $args );
 
 	return $str;
 });
 
 
 
-function renderHelp()
+
+class Form extends Forms
 {
-    return <<<EOT
+    public function __construct($lzy, $inx)
+    {
+        parent::__construct($lzy, $inx);
+    }
+
+
+
+    public function render( $headArgs )
+    {
+        $headElements = ['formName', 'id', 'mailto', 'mailfrom', 'mailTo', 'mailFrom', 'legend', 'customResponseEvaluation', 'next',
+            'confirmationText', 'file', 'warnLeavingPage', 'options', 'formTimeout', 'export', 'prefill',
+            'preventMultipleSubmit', 'replaceQuotes', 'antiSpam', 'validate', 'showData',
+            'showDataMinRows', 'encapsulate', 'disableCaching'];
+
+        // separate arguments for header and fields:
+        $formElems = [];
+        foreach ($headArgs as $key => $value) {
+            if (!in_array($key, $headElements)) {
+                $formElems[$key] = $value;
+                unset($headArgs[$key]);
+            }
+        }
+
+
+        // create form head:
+        $headArgs['type'] = 'form-head';
+        $str = parent::render( $headArgs );
+
+        // create form buttons:
+        $buttons = [ 'label' => '', 'type' => 'button', 'value' => '' ];
+
+        // parse further arguments, interpret as form field definitions:
+        foreach ($formElems as $label => $arg) {
+            if (is_string($arg)) {
+                $arg = ['type' => $arg ? $arg : 'text'];
+            }
+            if (isset($arg[0])) {
+                if ($arg[0] === 'required') {
+                    $arg['required'] = true;
+                    unset($arg[0]);
+                } else {
+                    $arg['type'] = $arg[0];
+                }
+            }
+            if ($label === 'submit') {
+                $buttons["label"] .= isset($arg['label']) ? $arg['label'].',': '{{ Submit }},';
+                $buttons["value"] .= 'submit,';
+                $arg['type'] = 'button';
+
+            } elseif ($label === 'cancel') {
+                $buttons["label"] .= isset($arg['label']) ? $arg['label'].',': '{{ Cancel }},';
+                $buttons["value"] .= 'cancel,';
+                $arg['type'] = 'button';
+
+            } elseif (strpos('formName,mailto,mailfrom,legend,showData', $label) !== false) {
+            } else {
+                $arg['label'] = $label;
+                $str .= parent::render($arg);
+            }
+        }
+        if ($buttons["label"]) {
+            $str .= parent::render($buttons);
+        }
+
+        $str .= parent::render([ 'type' => 'form-tail' ]);
+
+        return $str;
+    } // render
+
+
+
+    public function renderHelp()
+    {
+        return <<<EOT
 ## Help on macro form()
 form() is a short-hand for invoking form-head() and multiple form-elem(), terminated by form-tail().
 
@@ -104,8 +118,8 @@ Use the following syntax:
 {{ vgap }}
 Example:
 
-	'Name:': { type:text, required:true },
-	'E-Mail:': { email },    &#47;/ 'type' may be omitted
+    'Name:': { type:text, required:true },
+    'E-Mail:': { email },    &#47;/ 'type': may be omitted
 
 {{ vgap }}
 
@@ -122,5 +136,6 @@ Find details about arguments of form-head() and form-elem() below:
 {{ vgap }}
 {{ formelem( help ) }}
 EOT;
+    } // renderHelp
 
-}
+} // Class Form
