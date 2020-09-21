efa1cb32f740ba69a90674ee00901a902dbed23d
Dieter Stokar <zurisee@gmail.com>
2020-09-21 10:44:23 +0200

    bugfix: getArgsArray() earlier to avoid ghost arguments

--- macros/formelem.php
+++ macros/formelem.php
@@ -9,7 +9,8 @@ $macroName = basename(__FILE__, '.php');
 $this->addMacro($macroName, function () {
 	$macroName = basename(__FILE__, '.php');
 	$this->invocationCounter[$macroName] = (!isset($this->invocationCounter[$macroName])) ? 0 : ($this->invocationCounter[$macroName]+1);
-	$inx = $this->invocationCounter[$macroName] + 1;
+
+    $args = $this->getArgsArray($macroName);
 
     $label = $this->getArg($macroName, 'label', 'Label in front of the form field (mandatory).', '');
 
@@ -84,7 +85,6 @@ $this->addMacro($macroName, function () {
     $this->disablePageCaching = $this->getArg($macroName, 'disableCaching', '(false) Enables page caching (which is '.
         'disabled for this macro by default). Note: only active if system-wide caching is enabled.', true);
 
-	$args = $this->getArgsArray($macroName);
     if (isset($args['disableCaching'])) {
         unset($args['disableCaching']);
     }
