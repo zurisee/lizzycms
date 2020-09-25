feb1012dea437ceb8051b40056b259af9a85e6b2
Dieter Stokar <zurisee@gmail.com>
2020-09-25 11:29:09 +0200

    bugfix behavior in various contexts -> close all elems for radio and dropdown

--- js/reveal.js
+++ js/reveal.js
@@ -2,15 +2,36 @@
 
 
 $('.lzy-reveal-controller-elem').each(function() {
-	const tagName = $( this ).prop("tagName");
-	if ($( this ).prop("tagName") === 'SELECT') {
+	const $this = $( this );
+	const tagName = $this.prop("tagName");
+	var $target = null;
 
-	} else {
-		$(this).attr('aria-expanded', 'false');
-		const $target = $( $(this).attr('data-reveal-target') );
+	if ($this.prop("tagName") === 'SELECT') {		// case dropdown:
+		$('[data-reveal-target]', $this).each(function () {
+			$target = $( $(this).attr('data-reveal-target') );
+			if (!$target.parent().hasClass('lzy-reveal-container')) {
+				$target.wrap("<div class='lzy-reveal-container'></div>").show();
+				$target.css('margin-top', '-100vh');
+			}
+			if ( this.selected ) {
+				$target.parent().addClass('lzy-elem-revealed');
+				$(this).attr('aria-expanded', 'true');
+			} else {
+				$(this).attr('aria-expanded', 'false');
+			}
+		});
+
+	} else {											// case radio and checkbox:
+		$target = $( $this.attr('data-reveal-target') );
 		if (!$target.parent().hasClass('lzy-reveal-container')) {
-			$target.css('margin-top', '-100vh');
 			$target.wrap("<div class='lzy-reveal-container'></div>").show();
+			$target.css('margin-top', '-100vh');
+		}
+		if (this.checked) {
+			$this.attr('aria-expanded', 'true');
+			$target.parent().addClass('lzy-elem-revealed');
+		} else {
+			$this.attr('aria-expanded', 'false');
 		}
 	}
 });
@@ -19,28 +40,36 @@ $('.lzy-reveal-controller-elem').change(function() {
 	const $this = $( this );
 	var type = false;
 	var $target = false;
-	if ($this.prop("tagName") === 'SELECT') {
+
+	if ($this.prop("tagName") === 'SELECT') {				// case dropdown:
 		type = 'dropdown';
 		$target = $( $( ':selected', $this ).attr('data-reveal-target') );
-	} else {
+
+	} else {											// case radio and checkbox:
 		type = this.type;
 		$target = $( $this.attr('data-reveal-target') );
 	}
-	const boundingBox = $target[0].getBoundingClientRect();
-	$target.css('margin-top', (boundingBox.height * -1 - 10) + 'px');
 
-	if ( type === 'dropdown') { // case select: close all others
-		var $target1 = false;
-		$('option', $this).each(function() {
-			$target1 = $( $( this ).attr('data-reveal-target') );
-			$target1.parent().removeClass('lzy-elem-revealed');
-		});
-		// now open selected:
-		$target.parent().addClass('lzy-elem-revealed');
-		return;
+	// set margin-top according to elem height:
+	if ($target.length) {
+		const boundingBox = $target[0].getBoundingClientRect();
+		$target.css('margin-top', (boundingBox.height * -1 - 10) + 'px');
 	}
 
-	if (type === 'radio') { // case radio: close all others
+
+	if ( type === 'dropdown') {							// case select:
+		// var $target1 = false;
+		$('[data-reveal-target]', $this).each(function () {
+			$( $(this).attr('data-reveal-target') ).parent().removeClass('lzy-elem-revealed');
+			$(this).attr('aria-expanded', 'false');
+		});
+
+		// now open selected:
+		$target.parent().addClass('lzy-elem-revealed');
+		$this.attr('aria-expanded', 'true');
+		return;
+
+	} else if (type === 'radio') { 						// case radio: close all others
 		$this.parent().siblings().each(function() {
 			const $this1 = $('.lzy-reveal-controller-elem', $( this ));
 			const $target1 = $( $this1.attr('data-reveal-target') );
