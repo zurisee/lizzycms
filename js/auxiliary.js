44b2af8b71ceadc24872a808b2efe5dcb11db841
Dieter Stokar <zurisee@gmail.com>
2020-09-25 08:51:19 +0200

    bugfix: form info-icon -> preventDefault on click to avoid firing <a>

--- js/auxiliary.js
+++ js/auxiliary.js
@@ -15,6 +15,9 @@
             $('body').removeClass('lzy-small-screen').addClass('lzy-large-screen');
         }
     });
+    $('a.lzy-formelem-show-info').click( function ( e ) {
+        e.preventDefault();
+    });
 }( jQuery ));
 
 
