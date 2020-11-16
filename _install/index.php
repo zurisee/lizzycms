b0484c38ae2c1c022edda6c7051519e2092f09f7
sto <sto@usility.ch>
2020-11-16 15:17:30 +0100

    new initial installation procedure: _lizzy/_install/index.php -> copy to app-root and run -> will be replaced by operational index.php file

---
+++ _install/index.php
@@ -0,0 +1,35 @@
+<?php
+
+/***************************************************************************************************************
+ *  Lizzy Installation script
+ * -> to be executed right after downloading Lizzy in to _lizzy/
+ * -> this script will be overwritten by _lizzy/_parent-folder/index.php, which is the file for normal operation.
+ *
+ * For manual installation:
+ *  -> copy all files in _lizzy/_parent-folder/ to the app-root folder and
+ *  -> rename file 'htaccess' to '.htaccess'
+ **************************************************************************************************************/
+
+
+recursive_copy('_lizzy/_parent-folder/','./');
+rename('htaccess', '.htaccess');
+
+header("Location: ./");
+exit;
+
+
+function recursive_copy($src, $dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                recursive_copy($src .'/'. $file, $dst .'/'. $file);
+            }
+            else {
+                copy($src .'/'. $file,$dst .'/'. $file);
+            }
+        }
+    }
+    closedir($dir);
+}
