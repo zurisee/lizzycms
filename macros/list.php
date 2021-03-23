2c6f4f19ac77283bc3d3c00945a05867b60ba748
Dieter Stokar <zurisee@gmail.com>
2021-03-23 10:37:29 +0100

    macro list() - overhaul - 'registeredUsers'  re-introduced as supported keyword

--- macros/list.php
+++ macros/list.php
@@ -10,11 +10,10 @@ $this->addMacro($macroName, function () {
 	$this->invocationCounter[$macroName] = (!isset($this->invocationCounter[$macroName])) ? 0 : ($this->invocationCounter[$macroName]+1);
 	$inx = $this->invocationCounter[$macroName] + 1;
 
-	$source = $this->getArg($macroName, 'source', '[registeredUsers] Specifies what elements shall be listed.', '');
-    // $source = $this->getArg($macroName, 'source', '[registeredUsers,loggedInUsers] Specifies what elements shall be listed.', '');
+	$source = $this->getArg($macroName, 'source', '[list,registeredUsers,&#95;group&#95;] Comma-separated-string that shall be rendered as a list. List elements may contain keywords "<em>registeredUsers</em>" or "<em>&#95;group&#95;</em>" which selects users of given group.', '');
 
     if ($source === 'help') {
-        $this->getArg($macroName, 'text', '(optional) Comma-separated-string that shall be rendered as a list (alternative to arg "source")', '');
+        $this->getArg($macroName, 'text', 'Synonym for "source".', '');
         $this->getArg($macroName, 'prefix', '(optional) String added in front of every element.', '');
         $this->getArg($macroName, 'postfix', '(optional) String added after every element.', '');
         $this->getArg($macroName, 'separator', '(optional) String added between elements.', '');
@@ -22,11 +21,12 @@ $this->addMacro($macroName, function () {
         $this->getArg($macroName, 'wrapperClass', '(optional) Class to apply to each element.', '');
         $this->getArg($macroName, 'outerWrapperTag', '(optional) HTML tag to use as a wrapper around the rendered list of elements.', '');
         $this->getArg($macroName, 'outerWrapperClass', '(optional) Class to apply to wrapper.', '');
-        $this->getArg($macroName, 'options', '[capitalize] Modification to apply to every element.', '');
         $this->getArg($macroName, 'exclude', '(optional) Regex expression used to exclude specific elements from the list. E.g. "\\banon\\b"', '');
+        $this->getArg($macroName, 'capitalize', '(optional) Capitalizes each element.', '');
         $this->getArg($macroName, 'sort', '[ascending,descending] If defined, sorting is applied to the list of elements.', '');
-        $this->getArg($macroName, 'mode', '[ul,ol] Short-hand.', '');
-        $this->getArg($macroName, 'group', '(source=registeredUsers only) If defined, only users that are member of given group are listed.', '');
+        $this->getArg($macroName, 'mode', '[ul,ol] Short-hand to render as HTML lists.', '');
+        $this->getArg($macroName, 'options', '[capitalize,sort,ul,ol] Short-hand to invoke those options above.', '');
+        $this->getArg($macroName, 'emptyListPlaceholder', '(optional) Text rended if list turns out to be empty', '-- empty list --');
         $this->getArg($macroName, 'disableCaching', 'If true, page caching will be disabled (this may be useful in case of elements that may change over time, e.g. loggedInUsers).', false);
         return '';
     }
@@ -54,9 +54,12 @@ class OutputList
 
     public function render( $args )
     {
-        $source = @$args['source']? $args['source']: '<em>all</em>';
+        $source = @$args['source']? $args['source']: '';
+        $source = str_replace('registeredUsers', '<em>all</em>', $source);
         $text = @$args['text']? $args['text']: '';
+        $emptyListPlaceholder = @$args['emptyListPlaceholder']? $args['emptyListPlaceholder']: '-- empty list --';
         $out = '';
+
 
         if (preg_match_all('|<em>(.*?)</em>|', $source, $m)) {
             foreach ($m[1] as $i => $group) {
@@ -73,7 +76,7 @@ class OutputList
 
         $sort = @$args['sort']? $args['sort']: '';
         if ($sort) {
-            $elements = explode(',', $text);
+            $elements = explodeTrim(',', $text);
             if (($sort === true) || ($sort && ($sort[0] !== 'd'))) {
                 sort($elements, SORT_NATURAL | SORT_FLAG_CASE);
             } else {
@@ -84,6 +87,8 @@ class OutputList
 
         if($text) {
             $out .= renderList($text, $args);
+        } else {
+            $out = $emptyListPlaceholder;
         }
 
         return $out;
