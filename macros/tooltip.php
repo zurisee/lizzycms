1f510c27cc5376eb9947edaafd19a33eb5f99ffc
Dieter Stokar <zurisee@gmail.com>
2021-05-15 19:37:06 +0200

    tooltip(): various improvments, new args

--- macros/tooltip.php
+++ macros/tooltip.php
@@ -1,7 +1,8 @@
 <?php
 
 $page->addModules('TOOLTIPSTER');
-$page->addJq('$(".lzy-tooltip").tooltipster();');
+$page->addJq('$(".tooltip").tooltipster();');
+//$page->addJq('$(".lzy-tooltip").tooltipster();');
 
 $macroName = basename(__FILE__, '.php');
 $this->addMacro($macroName, function () {
@@ -11,12 +12,58 @@ $this->addMacro($macroName, function () {
 
 	$text = $this->getArg($macroName, 'text', 'Defines the visible text which shall get a tooltip.', '');
 	$description = $this->getArg($macroName, 'description', 'Defines the text that shall be shown inside the tooltip bubble.', '');
+	$contentFrom = $this->getArg($macroName, 'contentFrom', '[selector] If set, text from targeted element will be used as tooltip content.', '');
+	$id = $this->getArg($macroName, 'id', 'If defined, that identifier is applied to the DOM element as ID attribute.', '');
+	$class = $this->getArg($macroName, 'class', 'If defined, that identifier is added to the element class.', '');
+	$arrow = $this->getArg($macroName, 'arrow', 'If true, an arrow points from the tooltip to the anker text.', '');
+	$interactive = $this->getArg($macroName, 'interactive', 'If true, user can interact with the tooltip.', '');
+	$trigger = $this->getArg($macroName, 'trigger', '[hover,click,custom] Sets when the tooltip should open and close.', '');
+    $tooltipsterArgs = $this->getArg($macroName, 'tooltipsterArgs', 'String handed over to tooltipster() module as is.', '');
 
 	if ($text === 'help') {
 	    return '';
     }
 
-	$str = "<span class='lzy-tooltip lzy-tooltip-$inx' title='$description'>$text</span>";
+	if ($arrow) {
+        $tooltipsterArgs .= ", arrow:true,";
+    }
+	if ($interactive) {
+        $tooltipsterArgs .= ", interactive:true,";
+    }
+	if ($trigger) {
+        $tooltipsterArgs .= ", trigger:'$trigger',";
+    }
+	if ($contentFrom) {
+        $tooltipsterArgs .= ", content: $('$contentFrom').html(), contentAsHTML: true,";
+    }
+
+	if ($tooltipsterArgs || $id || $class || $contentFrom) {
+        $tooltipsterArgs = trim($tooltipsterArgs, ',');
+        $tooltipsterArgs = str_replace(',,', ',', $tooltipsterArgs);
+	    if (!preg_match('/^ { .*? } $/x', $tooltipsterArgs )) {
+            $tooltipsterArgs = "{ $tooltipsterArgs }";
+        }
+	    if ($id) {
+	        if ($id[0] === '#') {
+	            $id = substr($id, 1);
+            }
+            $this->page->addJq("$('#$id').tooltipster( $tooltipsterArgs );");
+	        $id = " id='$id'";
+
+        } elseif ($class) {
+            if ($class[0] === '.') {
+                $class = substr($class,1);
+            }
+            $this->page->addJq("$('.$class').tooltipster( $tooltipsterArgs );");
+
+        } else {
+	        $id = "lzy-tooltip-$inx";
+            $this->page->addJq("$('#$id').tooltipster( $tooltipsterArgs );");
+            $id = " id='$id'";
+        }
+        $class = $class? " $class": '';
+    }
+	$str = "<span$id class='lzy-tooltip lzy-tooltip-$inx$class' title='$description'>$text</span>";
 
 	$this->optionAddNoComment = true;
 	// $this->compileMd = true;
